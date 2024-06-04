6824e97696d4b9febcc9ebf331512d9ea441415b
Shihabul Hasan Shihab <6shihab@gmail.com>
2024-06-04 16:24:03 +0600

    Update common.sh

--- common.sh
+++ common.sh
@@ -13,9 +13,6 @@ sudo systemctl start docker
 sudo systemctl enable docker 
 sudo usermod -aG docker ${USER}
 
-sudo rm /etc/containerd/config.toml
-sudo systemctl restart containerd
-
 # install kubeadm
 
 
