8e0f7b92764217e4d018ca94c561fb76398ad0f6
Shihabul Hasan Shihab <6shihab@gmail.com>
2024-06-04 15:10:41 +0600

    master node shell command

--- master.sh
+++ master.sh
@@ -1,1 +1,12 @@
+sudo rm /etc/containerd/config.toml
+sudo systemctl restart containerd
 
+sudo kubeadm init
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+kubectl apply -f https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s.yaml
+
+sudo kubeadm token create --print-join-command
