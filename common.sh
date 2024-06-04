0789ae07331ea0ba3cd20aa4f6b4c433dec9da95
Shihabul Hasan Shihab <6shihab@gmail.com>
2024-06-04 15:07:04 +0600

    common shell script for master and worker node

---
+++ common.sh
@@ -0,0 +1,28 @@
+sudo swapoff -a
+sudo apt update
+
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt update
+apt-cache policy docker-ce
+sudo apt install -y docker-ce
+
+sudo systemctl start docker
+sudo systemctl enable docker 
+sudo usermod -aG docker ${USER}
+
+
+# install kubeadm
+
+
+sudo mkdir /etc/apt/keyrings/
+
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt update
+
+sudo apt install -y kubelet kubeadm kubectl kubernetes-cni
